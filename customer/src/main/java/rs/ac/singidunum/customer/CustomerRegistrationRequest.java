9875fe70ccde2e2c3eec3895b90770009007e9ab
nikola.jovanovic <nikola.jovanovic@devtechgroup.com>
2022-08-08 16:53:30 +0200

    first commit

---
+++ customer/src/main/java/rs/ac/singidunum/customer/CustomerRegistrationRequest.java
@@ -0,0 +1,8 @@
+package rs.ac.singidunum.customer;
+
+public record CustomerRegistrationRequest(
+                String firstName,
+                String lastName,
+                String email) {
+
+}
